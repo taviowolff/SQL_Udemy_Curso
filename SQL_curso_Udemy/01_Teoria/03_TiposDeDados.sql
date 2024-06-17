a5cffd16c4d5b76db957a21f4cf76496b9451577
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-06-17 12:48:16 -0300

    Create 03_TiposDeDados.sql

---
+++ SQL_curso_Udemy/01_Teoria/03_TiposDeDados.sql
@@ -0,0 +1,22 @@
+/* TIPOS DE DADOS*/
+
+/* CHAR - Armazena caracteres de tamanho fixo. Exemplo: CHAR(10) */
+/* Vai acumular espaços em branco para completar o tamanho definido */
+
+/* VARCHAR - Armazena caracteres de tamanho variável. Exemplo: VARCHAR(10) */
+/* Não acumula espaços em branco */
+
+/* Enum - */
+/* Permite que uma coluna contenha apenas um valor de um conjunto de valores pré-definidos */
+/* Exemplo: ENUM('M', 'F') */
+
+/* Tipos numéricos */
+/* INT - Armazena números inteiros */
+/* DECIMAL - Armazena números decimais */
+/* FLOAT - Armazena números decimais com ponto flutuante */
+
+/* 
+    algumas sgbds possuem tipos de dados específicos 
+    o MySQL recebe o dado do tipo ENUM
+    outras SGBDs não possuem o tipo ENUM
+*/
