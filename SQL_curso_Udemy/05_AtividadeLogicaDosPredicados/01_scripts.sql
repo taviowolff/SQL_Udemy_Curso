615e22b5a2ba48037adb7c033c58dc85623f325e
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-06-28 07:48:14 -0300

    Update 01_scripts.sql

--- SQL_curso_Udemy/05_AtividadeLogicaDosPredicados/01_scripts.sql
+++ SQL_curso_Udemy/05_AtividadeLogicaDosPredicados/01_scripts.sql
@@ -2,15 +2,28 @@
 --no departamento de filmes OU no
 --departamento de roupas
 
-SELECT departamento , count(idFuncionario) as 'Qtd funcionario'  FROM funcionarios WHERE departamento = 'Roupas' or departamento = 'Filmes' group by departamento;
+SELECT departamento , count(idFuncionario) as 'Qtd funcionario'  
+FROM funcionarios 
+WHERE departamento = 'Roupas' or departamento = 'Filmes' 
+group by departamento;
 
 --O gestor de marketing pediu a lista das funcionarias (AS) = FEMININO que trabalhem no departamento 
 --de filmes ou no departamento lar. Ele necessita enviar um email para as colaboradoras
 --desses dois setores. OR +  AND *
 
-SELECT nome, departamento, email from funcionarios WHERE (departamento = 'Filmes' or departamento = 'Lar') AND sexo = 'FEMININO';
+SELECT nome, departamento, email 
+FROM funcionarios 
+WHERE (departamento = 'Filmes' or departamento = 'Lar' ) 
+AND sexo = 'FEMININO';
+
+SELECT nome, departamento, email 
+FROM funcionarios 
+WHERE (departamento = 'Lar' AND sexo = 'FEMININO') 
+OR (departamento = 'Filmes' AND sexo = 'FEMININO');
 
 --Traga os funcionarios do sexo masculino
 --ou os funcionarios que trabalhem no setor Jardim
 
-SELECT nome, departamento, sexo FROM funcionarios WHERE sexo = 'MASCULINO' or departamento = 'Jardim';
+SELECT nome, departamento, sexo 
+FROM funcionarios 
+WHERE sexo = 'MASCULINO' or departamento = 'Jardim';
