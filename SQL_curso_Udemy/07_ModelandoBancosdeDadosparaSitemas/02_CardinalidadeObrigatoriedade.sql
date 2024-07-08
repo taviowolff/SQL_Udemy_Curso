9275d2fd160a9a315d427739e7e853fe7431d521
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-07-08 11:48:54 -0300

    Update 02_CardinalidadeObrigatoriedade.sql

--- SQL_curso_Udemy/07_ModelandoBancosdeDadosparaSitemas/02_CardinalidadeObrigatoriedade.sql
+++ SQL_curso_Udemy/07_ModelandoBancosdeDadosparaSitemas/02_CardinalidadeObrigatoriedade.sql
@@ -8,3 +8,10 @@ e cardinalidade dos dados dispostos em um banco de dados
 0,n (não obrigatório/mais de dado relacional)
 0,1 (não obirgatório/até um dado relacional)
 */
+
+CREATE TABLE Telfone (
+    IDtelefone INT PRIMARY KEY,
+    numero VARCHAR(20),
+    tipo VARCHAR(20),
+    FOREIGN KEY (IDcliente) REFERENCES(telefone)
+)
