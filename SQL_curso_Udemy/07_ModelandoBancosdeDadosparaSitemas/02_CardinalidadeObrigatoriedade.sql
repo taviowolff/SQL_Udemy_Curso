7a314d91a5f4399130e9ec720ad05e0aa1b1b078
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-07-08 11:39:03 -0300

    Create 02_CardinalidadeObrigatoriedade.sql

---
+++ SQL_curso_Udemy/07_ModelandoBancosdeDadosparaSitemas/02_CardinalidadeObrigatoriedade.sql
@@ -0,0 +1,10 @@
+/* Em SQL existem regras para as questões de obrigatoriedade
+e cardinalidade dos dados dispostos em um banco de dados
+*/
+
+/*
+1,1 (obrigatório/tendo apenas um dado relacional)
+1,n (obrigatório/mais de um dado relacional)
+0,n (não obrigatório/mais de dado relacional)
+0,1 (não obirgatório/até um dado relacional)
+*/
