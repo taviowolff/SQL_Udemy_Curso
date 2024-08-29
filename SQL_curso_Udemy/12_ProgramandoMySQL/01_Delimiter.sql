b7f2fca3128b5e2cf65abf22f0b1b74613509ac9
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-08-29 17:23:29 -0300

    Create 01_Delimiter.sql

---
+++ SQL_curso_Udemy/12_ProgramandoMySQL/01_Delimiter.sql
@@ -0,0 +1,10 @@
+-- Delimiter 
+-- Ou delimitador é o ; que vem por padrão em todos os SGBDs 
+-- Serve para dizer ao SGBD onde o seu comando acaba
+
+-- Para programa em SQL é necessário trocar esse delimiter 
+
+-- para mudar o delimiter
+-- Escreva no command line do MySQL e defina qual você quer
+
+-- Delimiter $
