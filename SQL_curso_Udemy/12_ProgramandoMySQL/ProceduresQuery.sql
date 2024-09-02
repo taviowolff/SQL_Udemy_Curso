a7faa3a1e598b2f44d14900b630f0a16bf96c053
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-09-02 17:13:35 -0300

    Create ProceduresQuery.sql

---
+++ SQL_curso_Udemy/12_ProgramandoMySQL/ProceduresQuery.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE FACULDADE;
+
+CREATE TABLE CURSOS (
+    ID_CURSO INT PRIMARY KEY AUTO_INCREMENT,
+    NOME VARCHAR (30) NOT NULL,
+    HORAS INT(3) NOT NULL,
+    PRECO FLOAT(10.2)
+);
+
+INSERT INTO CURSOS VALUES (NULL, 'JAVA', 35, 1000.00);
+INSERT INTO CURSOS VALUES (NULL, 'PYTHON', 40, 1200.00);
+
+SELECT * FROM CURSOS;
+
+DELIMITER //
+
+CREATE PROCEDURE CAD_CURSO (P_NOME VARCHAR(30),
+                            P_HORAS INT(3),
+                            P_PRECO FLOAT(10.2))
+BEGIN
+
+    INSERT INTO CURSOS VALUES (NULL, P_NOME, P_HORAS, P_PRECO);
+
+END
+//
+
+DELIMITER ;
+
+CALL CAD_CURSO('EXCEL', 30, 900.00);
+
++----------+--------+-------+-------+
+| ID_CURSO | NOME   | HORAS | PRECO |
++----------+--------+-------+-------+
+|        1 | JAVA   |    35 |  1000 |
+|        2 | PYTHON |    40 |  1200 |
+|        3 | EXCEL  |    30 |   900 |
++----------+--------+-------+-------+
+
+-- DEVER -> CRIAR UMA PROCEDURE PARA CONSULTAR CURSOS
+
