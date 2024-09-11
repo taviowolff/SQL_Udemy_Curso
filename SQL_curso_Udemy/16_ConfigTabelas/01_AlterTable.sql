fdf582af67e65af63cf0592506f4b4bbaeae3260
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-09-11 14:12:04 -0300

    Create 01_AlterTable.sql

---
+++ SQL_curso_Udemy/16_ConfigTabelas/01_AlterTable.sql
@@ -0,0 +1,104 @@
+/* ALTERANDO TABLEAS */
+
+CREATE TABLE TABELA (
+    COLUNA1 VARCHAR(30),
+    COLUNA2 VARCHAR(30),
+    COLUNA3 VARCHAR(30)
+);
+-- Query OK, 0 rows affected (0.03 sec)
+
+
+DESC TABELA;
++---------+-------------+------+-----+---------+-------+
+| Field   | Type        | Null | Key | Default | Extra |
++---------+-------------+------+-----+---------+-------+
+| COLUNA1 | varchar(30) | YES  |     | NULL    |       |
+| COLUNA2 | varchar(30) | YES  |     | NULL    |       |
+| COLUNA3 | varchar(30) | YES  |     | NULL    |       |
++---------+-------------+------+-----+---------+-------+
+
+
+-- ADICIONANDO MA PK 
+
+ALTER TABLE TABELA
+ADD PRIMARY KEY (COLUNA1);
+-- Query OK, 0 rows affected (0.06 sec)
+
+DESC TABELA;
++---------+-------------+------+-----+---------+-------+
+| Field   | Type        | Null | Key | Default | Extra |
++---------+-------------+------+-----+---------+-------+
+| COLUNA1 | varchar(30) | NO   | PRI | NULL    |       |
+| COLUNA2 | varchar(30) | YES  |     | NULL    |       |
+| COLUNA3 | varchar(30) | YES  |     | NULL    |       |
++---------+-------------+------+-----+---------+-------+
+
+
+-- ADICIONANDO COLUNA SEM POSICAO. VAI PARA ULTIMA POSICAO 
+
+ALTER TABLE TABELA
+ADD COLUNA VARCHAR(30);
+-- Query OK, 0 rows affected (0.02 sec)
+-- Records: 0  Duplicates: 0  Warnings: 0
+
+DESC TABELA;
++---------+-------------+------+-----+---------+-------+
+| Field   | Type        | Null | Key | Default | Extra |
++---------+-------------+------+-----+---------+-------+
+| COLUNA1 | varchar(30) | NO   | PRI | NULL    |       |
+| COLUNA2 | varchar(30) | YES  |     | NULL    |       |
+| COLUNA3 | varchar(30) | YES  |     | NULL    |       |
+| COLUNA  | varchar(30) | YES  |     | NULL    |       |
++---------+-------------+------+-----+---------+-------+
+
+ALTER TABLE TABELA
+ADD COLUNA4 VARCHAR(30) NOT NULL UNIQUE
+AFTER COLUNA3;
+
+
++---------+-------------+------+-----+---------+-------+
+| Field   | Type        | Null | Key | Default | Extra |
++---------+-------------+------+-----+---------+-------+
+| COLUNA1 | varchar(30) | NO   | PRI | NULL    |       |
+| COLUNA2 | varchar(30) | YES  |     | NULL    |       |
+| COLUNA3 | varchar(30) | YES  |     | NULL    |       |
+| COLUNA4 | varchar(30) | NO   | UNI | NULL    |       |
+| COLUNA  | varchar(30) | YES  |     | NULL    |       |
++---------+-------------+------+-----+---------+-------+
+
+
+-- MODIFICANDO O TIPO DE UM CAMPO
+ALTER TABLE TABELA MODIFY COLUNA2 DATE NOT NULL;
+
++---------+-------------+------+-----+---------+-------+
+| Field   | Type        | Null | Key | Default | Extra |
++---------+-------------+------+-----+---------+-------+
+| COLUNA1 | varchar(30) | NO   | PRI | NULL    |       |
+| COLUNA2 | date        | NO   |     | NULL    |       |
+| COLUNA3 | varchar(30) | YES  |     | NULL    |       |
+| COLUNA4 | varchar(30) | NO   | UNI | NULL    |       |
+| COLUNA  | varchar(30) | YES  |     | NULL    |       |
++---------+-------------+------+-----+---------+-------+
+
+-- RENOMEANDO O NOME DA TABELA
+ALTER TABLE TABELA
+RENAME PESSOA;
+
+
+CREATE TABLE TIME(
+    IDTIME INT PRIMARY KEY AUTO_INCREMENT,
+    TIME VARCHAR(30),
+    ID_PESSOA VARCHAR(30)
+);
+
+-- FOREIGN KEY
+ALTER TABLE TIME
+ADD FOREIGN KEY(ID_PESSOA)
+REFERENCES PESSOA(COLUNA1);
+
+
+
+
+
+
+
